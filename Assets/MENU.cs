ad4940fb68888103b8a9caff40fed757548cdb9d
FizaBibi <fizabibi122@gmail.com>
2023-08-15 05:05:49 -1200

    Addition of main menu

---
+++ Assets/MENU.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MENU : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   public void playgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
