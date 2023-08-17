fd615b6c71379cc02da765ff0079e6051b8f13be
Haroon Sohail <109343967+h4roons@users.noreply.github.com>
2023-08-17 19:03:39 +0500

    added navmesh

---
+++ Assets/navmeshmove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navmeshmove : MonoBehaviour
+{
+    public GameObject Singer;
+
+    public NavMeshAgent crowd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        crowd.SetDestination(Singer.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
