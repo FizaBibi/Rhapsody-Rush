9b03768a1f0904f4e2544bd85a1c73e61fd38b65
Fawad <fawadshah501@gmail.com>
2023-08-24 03:09:04 +0500

    added blood particle effect

--- Assets/Script/Spike.cs
+++ Assets/Script/Spike.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem bloodParticle;
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("hehehe " + collision.gameObject.name);
         if (collision.gameObject.CompareTag("enemy"))
+        {
+            //bloodParticle.transform.position = ;
+             Instantiate(bloodParticle, collision.gameObject.transform.position, bloodParticle.transform.rotation);
+            bloodParticle.Play();
             Destroy(collision.gameObject);
-
-        // here
-
+        }
     }
 }
--- Assets/navmeshmove.cs
+++ Assets/navmeshmove.cs
@@ -6,7 +6,6 @@ using UnityEngine.AI;
 public class navmeshmove : MonoBehaviour
 {
     public GameObject Singer;
-
     public NavMeshAgent crowd;
     // Start is called before the first frame update
     void Start()
