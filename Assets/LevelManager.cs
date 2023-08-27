e6e3ed443d525c7a25c5ff133728dccb2ca76204
Fawad <fawadshah501@gmail.com>
2023-08-27 23:10:56 +0500

    Music Issue resolved

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,32 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject Win;
+    public GameObject WinUi;
     public GameObject bg;
-    public GameObject GameOver;
+    public GameObject GameWinLoseMusic;
     public GameObject BgMusic;
-    public GameObject WinMusic;   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.transform.childCount==0)
+        if (this.transform.childCount == 0)
         {
-            Win.SetActive(true);
-            GameOver.SetActive(false);
+            WinUi.SetActive(true);
+            GameWinLoseMusic.SetActive(true);
             bg.SetActive(false);
             BgMusic.SetActive(false);
-            WinMusic.SetActive(true);
-
         }
     }
-    
 }
